ccb08c3f259cf949b23309cdc1fca391d3ccf922
gwansikk <seorit98@gmail.com>
2023-06-26 15:39:09 +0900

    * Create VRT

---
+++ VRT/README.md
@@ -0,0 +1,5 @@
+# Vite React TailwindCSS
+
+Vite + React + TailwindCSS의 기본 템플릿입니다.
+
+## Installed
---
+++ VRT/src/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div className="flex">
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+
+      <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+}
+
+export default App;
